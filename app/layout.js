30b052fe158a8f1212b0192f3ca20cb7be0d55b1
Micaela Korol <micaelakorol@gmail.com>
2024-03-08 15:46:12 -0300

    logo added to tab

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,27 @@
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import { logo } from "@/public/assets/Icons";
 
-const dm_sans = DM_Sans({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
+const dm_sans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata = {
-  title: "Herrería Javier Page",
-  description: "Herreria de obra, realizamos puertas, rejas, portones de todo tipo y tamaño .. ",
+  title: "Herrería Javier",
+  description:
+    "Herreria de obra, realizamos puertas, rejas, portones de todo tipo y tamaño .. ",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={dm_sans.className}>{children}</body>
-    </html>
+    <>
+      <head>
+        <link rel="icon" href={logo} />
+      </head>
+      <html lang="es">
+        <body className={dm_sans.className}>{children}</body>
+      </html>
+    </>
   );
 }
